dc42f6be4170f74af9f742353b6b30295d9d5b5d
Turtlesound <153922951+Turtlesound@users.noreply.github.com>
2025-10-29 14:39:34 +0100

    Update start.asm
    fixed so ./snake_asm_start 5 2 also works

--- assignment_3/start.asm
+++ assignment_3/start.asm
@@ -29,14 +29,19 @@ _start:
     # rsp+16: argv[1]
     # rsp+24: argv[2]
     
-    popq    %rdi                    # argc
-    movq    %rsp, %rsi              # argv
+    # At program entry, stack is 16-byte aligned
+    # We need to keep it aligned for function calls
+    
+    movq    (%rsp), %rdi            # argc (without popping)
+    leaq    8(%rsp), %rsi           # argv (pointer to first element)
     
     # Check if we have enough arguments
     cmpq    $3, %rdi
     jge     parse_args
     
     # Print usage message
+    # Need to save argv for later, and align stack
+    pushq   %rsi                    # Save argv (also aligns stack for call)
     movq    (%rsi), %rsi            # argv[0] (program name)
     leaq    usage_msg(%rip), %rdi
     xorl    %eax, %eax
@@ -47,24 +52,46 @@ _start:
     call    exit
     
 parse_args:
-    # Save argv pointer
-    pushq   %rsi
+    # Save argv pointer and align stack
+    # We need to preserve argv across calls and maintain alignment
+    pushq   %rbx                    # Callee-saved register for length
+    pushq   %r12                    # Callee-saved register for apples
+    pushq   %rsi                    # Save argv pointer
+    
+    # Stack is now misaligned (pushed 3 times = 24 bytes)
+    # Need one more push to align to 16 bytes
+    pushq   %rsi                    # Dummy push for alignment
+    
+    # Stack layout now:
+    # 0(%rsp) = dummy rsi
+    # 8(%rsp) = saved rsi (argv pointer)
+    # 16(%rsp) = saved r12
+    # 24(%rsp) = saved rbx
     
     # Parse first argument (snake length)
-    movq    8(%rsi), %rdi           # argv[1]
+    movq    8(%rsp), %rax           # Get saved argv from stack
+    movq    8(%rax), %rdi           # argv[1] - pointer to first arg string
     call    atoi
     movl    %eax, %ebx              # Save length in ebx
     
     # Parse second argument (number of apples)
-    popq    %rsi
-    movq    16(%rsi), %rdi          # argv[2]
+    movq    8(%rsp), %rax           # Get saved argv from stack
+    movq    16(%rax), %rdi          # argv[2] - pointer to second arg string
     call    atoi
-    movl    %eax, %ecx              # Save apples in ecx
+    movl    %eax, %r12d             # Save apples in r12d
     
-    # Call start_game
+    # Clean up alignment padding
+    addq    $8, %rsp
+    popq    %rsi                    # Restore (but we don't need it anymore)
+    
+    # Call start_game with 16-byte aligned stack
     movl    %ebx, %edi              # First parameter: length
-    movl    %ecx, %esi              # Second parameter: apples
+    movl    %r12d, %esi             # Second parameter: apples
     call    start_game
+    
+    # Restore callee-saved registers
+    popq    %r12
+    popq    %rbx
     
     # Exit with code 0
     xorl    %edi, %edi
